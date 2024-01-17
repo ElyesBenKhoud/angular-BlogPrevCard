7bb9e90ee32dd12fd798081a2f529c4a27fac38a
Elyes.Benkhoud <elyesbenkhoud377@gmail.com>
2024-01-17 09:51:59 +0200

    distructuring components

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { CardContentComponent } from './card-content/card-content.component';
+import { CardFooterComponent } from './card-footer/card-footer.component';
 @Component({
   selector: 'app-root',
-  imports: [CardContentComponent],
+  imports: [CardContentComponent, CardFooterComponent],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
---
+++ app/src/app/card-footer/card-footer.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-footer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card-footer.component.html',
+  styleUrl: './card-footer.component.css',
+})
+export class CardFooterComponent {}
